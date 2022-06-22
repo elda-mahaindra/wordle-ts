033fb9d48ae5c9d34aac91e8953bc52486fa788b
elda-mahaindra <faith030@gmail.com>
2022-06-22 17:17:13 +0700

    intro & setup

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+// ---------------------------------------------- modules import
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+// ---------------------------------------------- app render
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
