6821feddc92608cb14f91407f78eec9f1454009c
elda-mahaindra <faith030@gmail.com>
2022-06-23 11:00:21 +0700

    submitting guesses

--- src/components/hooks/useWordle.ts
+++ src/components/hooks/useWordle.ts
@@ -16,7 +16,9 @@ export const useWordle = (solution: ISolution) => {
   // ---------------------------------------------- private methods
   // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("formatting the guess - ", currentGuess);
+  };
 
   // add a new guess to the guesses state
   // update the isCorrect state if the guess is correct
@@ -26,6 +28,26 @@ export const useWordle = (solution: ISolution) => {
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }: { key: string }) => {
+    if (key === "Enter") {
+      // only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log("you used all your guesses!");
+        return;
+      }
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("you already tried that word.");
+        return;
+      }
+      // check word is 5 chars
+      if (currentGuess.length !== 5) {
+        console.log("word must be 5 chars.");
+        return;
+      }
+
+      formatGuess();
+    }
+
     if (key === "Backspace") {
       setCurrentGuess((prev) => prev.slice(0, -1));
       return;
